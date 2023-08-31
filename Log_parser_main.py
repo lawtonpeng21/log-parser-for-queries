18a3bf6641a271a7a793e895b9080a125d4f2683
Lawton Peng <liangzipeng2004@gmail.com>
2023-08-31 14:45:52 -0500

    Removed main from module

--- Log_parser_main.py
+++ Log_parser_main.py
@@ -1,31 +1,26 @@
-# Imports necessary libraries
 import os
-from .log_parser_module import LogParser
+import sys
+from log_parser_module import LogParser  # Import LogParser directly
 
-# Defines the main function
+# Add the directory containing log_parser_module.py to the Python path
+module_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(module_dir)
+
 def main():
-    # Defines the path to the folder containing log files
     log_folder_path = r"C:\Users\m295939\Desktop\New folder"  # Replace with the actual folder path
-
-    # Defines the output folder for parsed logs
     output_folder = os.path.join(os.path.expanduser("~"), "Desktop", "parsed_logs")
 
-    # Creates the output folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Creates an instance of the LogParser class
     log_parser = LogParser(log_folder_path, output_folder)
 
-    # Defines patterns and keywords for log extraction
     keyword = "-DEBUG in i.u.i.o.a.r.impl.QuestQueryProcessor - SPARQL query:"
     timestamp_pattern = r'\d{2}:\d{2}:\d{2}\.\d{3}'
     date_pattern = r'\d{4}-\d{2}-\d{2}'
 
-    # Extracts information from log files using the LogParser instance
     log_parser.extract_information(keyword, timestamp_pattern, date_pattern)
 
-# Checks if the script is being run directly
 if __name__ == "__main__":
-    # Calls the main function to start the log parsing process
     main()
+
